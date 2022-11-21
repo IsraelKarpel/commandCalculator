fab93ccd4bbc44fd4beb58023903859b6a71143d
israelkarpel <israel@jobtestprep.com>
2022-11-21 11:44:20 +0200

    Api and multipule operators

--- calculator.js
+++ calculator.js
@@ -1,10 +1,28 @@
-
 class Calculator {
   current = 0;
+  commands = [];
 
-  execute(command) {
-   this.current = command.execute(this.current, command.value);
-   console.log('The result is ', this.current)
+  constructor(number) {
+    this.current = parseInt(number);
+  }
+
+  pushCommand(command) {
+    this.commands.push(command);
+  }
+
+  execute() {
+    this.commands.forEach((command) => {
+      this.current = command.execute(this.current, command.value);
+    });
+    this.commands = [];
+  }
+
+  undo() {
+    this.commands.pop();
+  }
+
+  getResult() {
+    console.log("The result is ", this.current);
   }
 }
 
